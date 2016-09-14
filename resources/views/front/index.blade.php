45dc5c786900cf2ccf5d657bf4c09f22f2f3e919
phan ha <hapt@qsoft.com.vn>
2016-09-14 11:04:27 +0700

    add layout

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -1,21 +1,239 @@
 @extends('front.template')
 
-@section('main')
-	<div class="row">
-		<div class="box">
-			<div class="col-lg-12">
-				<hr>
-				<h2 class="intro-text text-center"><strong>Comitem</strong></h2>
-				<hr>
-				<img class="img-responsive img-left" src="img/laravel-l-slant.png" alt="">
-				<p>Postremo ad id indignitatis est ventum, ut cum peregrini ob formidatam haut ita dudum alimentorum inopiam pellerentur ab urbe praecipites, sectatoribus disciplinarum liberalium inpendio paucis sine respiratione ulla extrusis, tenerentur minimarum adseclae veri, quique id simularunt ad tempus, et tria milia saltatricum ne interpellata quidem cum choris.</p>
-				<p>Quo cognito Constantius ultra mortalem modum exarsit ac nequo casu idem Gallus de futuris incertus agitare quaedam conducentia saluti suae per itinera conaretur, remoti sunt omnes.</p>
-				<p>Eminuit autem inter humilia supergressa iam impotentia fines mediocrium delictorum nefanda Clematii cuiusdam Alexandrini nobilis mors repentina; cuius socrus cum misceri sibi generum, flagrans eius amore, non impetraret, ut ferebatur, per palatii pseudothyrum introducta, oblato pretioso reginae monili id adsecuta est, ut ad Honoratum tum comitem orientis formula missa letali omnino scelere nullo contactus idem Clematius nec hiscere nec loqui permissus occideretur.</p>
-				<p>Cuius acerbitati uxor grave accesserat incentivum, germanitate Augusti turgida supra modum, quam Hannibaliano regi fratris filio antehac Constantinus iunxerat pater, Megaera quaedam mortalis, inflammatrix saevientis adsidua, humani cruoris avida nihil mitius quam maritus; qui paulatim eruditiores facti processu temporis ad nocendum per clandestinos versutosque rumigerulos conpertis leviter addere quaedam male suetos falsa et placentia sibi discentes, adfectati regni vel artium nefandarum calumnias insontibus adfligebant.</p>
-				<p>Montius nos tumore inusitato quodam et novo ut rebellis et maiestati recalcitrantes Augustae per haec quae strepit incusat iratus nimirum quod contumacem praefectum, quid rerum ordo postulat ignorare dissimulantem formidine tenus iusserim custodiri.</p>
-			</div>
-		</div>
-	</div>
+{{--for yeild--}}
+@section('content')
+    <div id="content">
+        <div class="content">
+            <div class="wrapper_1">
+                <div class="page_slider">
+                    <div class="slider slider_2">
+                        <div class="slide">
+                            <img src="http://lorempixel.com/400/200/" alt=""/>
+                            <div class="description">
+                                <h2 class="title">2012 Mercedes-Benz CLS 320</h2>
+                                <p class="desc">
+                                    <span><strong>Miles: </strong>25,000</span><span><strong>Engine: </strong>2.6</span>
+                                </p>
+                                <div class="price">$ 32 200</div>
+                            </div>
+                        </div>
+                        <div class="slide">
+                            <img src="http://lorempixel.com/400/200/" alt=""/>
+                            <div class="description">
+                                <h2 class="title">2010 Mercedes-Benz Sport</h2>
+                                <p class="desc">
+                                    <span><strong>Miles: </strong>25,000</span><span><strong>Engine: </strong>2.6</span>
+                                </p>
+                                <div class="price">$ 32 200</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="wrapper_4">
+                <div class="">
+                    <div class="recent_cars four_columns">
+                        <h2><strong>Cars</strong> gallery</h2>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <img src="http://lorempixel.com/400/200/" alt=""/>
+                                    <div class="description">Registration 2010<br/>3.0 Diesel<br/>230 HP<br/>Body
+                                        Coupe<br/>80 000 Miles
+                                    </div>
+                                    <div class="title">Mercedes-Benz <span class="price">$ 115 265</span></div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="http://lorempixel.com/400/200/" alt=""/>
+                                    <div class="description">Registration 2010<br/>3.0 Diesel<br/>230 HP<br/>Body
+                                        Coupe<br/>80 000 Miles
+                                    </div>
+                                    <div class="title">Mercedes-Benz <span class="price">$ 115 265</span></div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="images/placeholders/220x164.gif" alt=""/>
+                                    <div class="description">Registration 2010<br/>3.0 Diesel<br/>230 HP<br/>Body
+                                        Coupe<br/>80 000 Miles
+                                    </div>
+                                    <div class="title">Mercedes-Benz <span class="price">$ 115 265</span></div>
+                                </a>
+                            </li>
+                            <li class="last">
+                                <a href="#">
+                                    <img src="images/placeholders/220x164.gif" alt=""/>
+                                    <div class="description">Registration 2010<br/>3.0 Diesel<br/>230 HP<br/>Body
+                                        Coupe<br/>80 000 Miles
+                                    </div>
+                                    <div class="title">Mercedes-Benz <span class="price">$ 115 265</span></div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="images/placeholders/220x164.gif" alt=""/>
+                                    <div class="description">Registration 2010<br/>3.0 Diesel<br/>230 HP<br/>Body
+                                        Coupe<br/>80 000 Miles
+                                    </div>
+                                    <div class="title">Mercedes-Benz <span class="price">$ 115 265</span></div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="images/placeholders/220x164.gif" alt=""/>
+                                    <div class="description">Registration 2010<br/>3.0 Diesel<br/>230 HP<br/>Body
+                                        Coupe<br/>80 000 Miles
+                                    </div>
+                                    <div class="title">Mercedes-Benz <span class="price">$ 115 265</span></div>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="wrapper_3">
+                <div class="left">
+                    <div class="recent_cars">
+                        <h2><strong>Recent</strong> listings</h2>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <img src="images/placeholders/220x164.gif" alt=""/>
+                                    <div class="description">Registration 2010<br/>3.0 Diesel<br/>230 HP<br/>Body
+                                        Coupe<br/>80 000 Miles
+                                    </div>
+                                    <div class="title">Mercedes-Benz <span class="price">$ 115 265</span></div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="images/placeholders/220x164.gif" alt=""/>
+                                    <div class="description">Registration 2010<br/>3.0 Diesel<br/>230 HP<br/>Body
+                                        Coupe<br/>80 000 Miles
+                                    </div>
+                                    <div class="title">Mercedes-Benz <span class="price">$ 115 265</span></div>
+                                </a>
+                            </li>
+                            <li class="last">
+                                <a href="#">
+                                    <img src="images/placeholders/220x164.gif" alt=""/>
+                                    <div class="description">Registration 2010<br/>3.0 Diesel<br/>230 HP<br/>Body
+                                        Coupe<br/>80 000 Miles
+                                    </div>
+                                    <div class="title">Mercedes-Benz <span class="price">$ 115 265</span></div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="images/placeholders/220x164.gif" alt=""/>
+                                    <div class="description">Registration 2010<br/>3.0 Diesel<br/>230 HP<br/>Body
+                                        Coupe<br/>80 000 Miles
+                                    </div>
+                                    <div class="title">Mercedes-Benz <span class="price">$ 115 265</span></div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="images/placeholders/220x164.gif" alt=""/>
+                                    <div class="description">Registration 2010<br/>3.0 Diesel<br/>230 HP<br/>Body
+                                        Coupe<br/>80 000 Miles
+                                    </div>
+                                    <div class="title">Mercedes-Benz <span class="price">$ 115 265</span></div>
+                                </a>
+                            </li>
+                            <li class="last">
+                                <a href="#">
+                                    <img src="images/placeholders/220x164.gif" alt=""/>
+                                    <div class="description">Registration 2010<br/>3.0 Diesel<br/>230 HP<br/>Body
+                                        Coupe<br/>80 000 Miles
+                                    </div>
+                                    <div class="title">Mercedes-Benz <span class="price">$ 115 265</span></div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="images/placeholders/220x164.gif" alt=""/>
+                                    <div class="description">Registration 2010<br/>3.0 Diesel<br/>230 HP<br/>Body
+                                        Coupe<br/>80 000 Miles
+                                    </div>
+                                    <div class="title">Mercedes-Benz <span class="price">$ 115 265</span></div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="images/placeholders/220x164.gif" alt=""/>
+                                    <div class="description">Registration 2010<br/>3.0 Diesel<br/>230 HP<br/>Body
+                                        Coupe<br/>80 000 Miles
+                                    </div>
+                                    <div class="title">Mercedes-Benz <span class="price">$ 115 265</span></div>
+                                </a>
+                            </li>
+                            <li class="last">
+                                <a href="#">
+                                    <img src="images/placeholders/220x164.gif" alt=""/>
+                                    <div class="description">Registration 2010<br/>3.0 Diesel<br/>230 HP<br/>Body
+                                        Coupe<br/>80 000 Miles
+                                    </div>
+                                    <div class="title">Mercedes-Benz <span class="price">$ 115 265</span></div>
+                                </a>
+                            </li>
+                        </ul>
+                        <a href="#" class="all">all listings</a>
+                    </div>
+                </div>
+                <div class="right">
+                    <div class="news">
+                        <h2><strong>Auto</strong> news</h2>
+                        <div class="news_box">
+                            <a href="#" class="thumb">
+                                <img src="images/placeholders/180x135.gif" alt=""/>
+                            </a>
+                            <h5><a href="#">UNOFFICIAL PORSCHE 918 SPYDER PRICING POPS UP</a></h5>
+                            <div class="date">November 1, 2012</div>
+                            <div class="post">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit... </p>
+                            </div>
+                        </div>
+                        <div class="news_box bottom">
+                            <a href="#" class="thumb">
+                                <img src="images/placeholders/180x135.gif" alt=""/>
+                            </a>
+                            <h5><a href="#">UNOFFICIAL PORSCHE 918 SPYDER PRICING POPS UP</a></h5>
+                            <div class="date">November 1, 2012</div>
+                            <div class="post">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit... </p>
+                            </div>
+                        </div>
+                        <div class="all_wrapper"><a href="#" class="all_news">All news</a></div>
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div class="services">
+                <div class="service service_1">
+                    <h2><strong>Delivery</strong> on time</h2>
+                    <p>Lorem ipsum dolo stet consectetur adipiscin elit. Vestibulu dictum, vulputate ullamcoper lorem
+                        ipsum dolo stet.</p>
+                    <a href="#" class="more">More</a>
+                </div>
+                <div class="service service_2 center">
+                    <h2><strong>Quality</strong> service</h2>
+                    <p>Lorem ipsum dolo stet consectetur adipiscin elit. Vestibulu dictum, vulputate ullamcoper lorem
+                        ipsum dolo stet.</p>
+                    <a href="#" class="more">More</a>
+                </div>
+                <div class="service service_3">
+                    <h2><strong>Retro</strong> cars</h2>
+                    <p>Lorem ipsum dolo stet consectetur adipiscin elit. Vestibulu dictum, vulputate ullamcoper lorem
+                        ipsum dolo stet.</p>
+                    <a href="#" class="more">More</a>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @stop
 
--- resources/views/front/template.blade.php
+++ resources/views/front/template.blade.php
@@ -1,145 +1,103 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-	<head>
+        <!-- Facebook APP ID -->
+        <meta property="fb:app_id" content="12345"/>
 
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title>{{ trans('front/site.title') }}</title>
-		<meta name="description" content="">	
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="keywords" content="Car-Dealer, auto-salon, automobile, business, car, car-gallery, car-selling-template, cars, dealer, marketplace, mobile, real estate, responsive, sell, vehicle" />
+        <meta name="description" content="Auto Dealer HTML - Responsive HTML Auto Dealer Template" />
 
-		@yield('head')
+        <!-- Open Graph -->
+        <meta property="og:site_name" content="Auto Dealer HTML"/>
+        <meta property="og:title" content="Gallery" />
+        <meta property="og:url" content="http://localhost/12_gallery.html" />
+        <meta property="og:image" content="http://cdn.winterjuice.com/example/autodealer/image.jpg" />
+        <meta property="og:description" content="{{ trans('front/site.title') }}" />
 
-		{!! HTML::style('css/main_front.css') !!}
+        <!-- Page Title -->
+        <title>{{ trans('front/site.title') }}</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <link rel="stylesheet" type="text/css" href="css/style980.css" />
+        <link rel="stylesheet" type="text/css" href="css/style800.css" />
+        <link rel="stylesheet" type="text/css" href="css/style700.css" />
+        <link rel="stylesheet" type="text/css" href="css/style600.css" />
+        <link rel="stylesheet" type="text/css" href="css/style500.css" />
+        <link rel="stylesheet" type="text/css" href="css/style400.css" />
+        <link rel="stylesheet" type="text/css" href="css/jquery.fancybox-1.3.4.css" media="screen" />
+        <!--[if IE]>
+        <link href="css/style_ie.css" rel="stylesheet" type="text/css">
+        <![endif]-->
+        <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+        <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+        <script type="text/javascript" src="js/jquery.bxslider.js"></script>
+        <script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+        <script type="text/javascript" src="js/jquery.selectik.js"></script>
+        <script type="text/javascript" src="js/jquery.mousewheel-3.0.4.pack.js"></script>
+        <script type="text/javascript" src="js/jquery.fancybox-1.3.4.pack.js"></script>
+        <script type="text/javascript" src="js/jquery.countdown.js"></script>
+        <script type="text/javascript" src="js/jquery.checkbox.js"></script>
+        <script type="text/javascript" src="js/js.js"></script>
+    </head>
+    <body class="index">
+    <!--BEGIN HEADER-->
+    <div id="header">
+        <div class="top_info">
+            <div class="logo">
+                <a href="#">Example<span>Exam</span></a>
+            </div>
+            <div class="header_contacts">
+                <div class="phone">01234657980</div>
+                <div>WinterJuice, LLC, 1875 South Grant Street, Suite 680, San Mateo, CA 94402</div>
+            </div>
+            <div class="socials">
+                <a href="#"><img src="images/fb_icon.png" alt=""></a>
+                <a href="#"><img src="images/twitter_icon.png" alt=""></a>
+                <a href="#"><img src="images/in_icon.png" alt=""></a>
+            </div>
+        </div>
+        <div class="bg_navigation">
+            <div class="navigation_wrapper">
+                <div id="navigation">
+                    <span>Home</span>
+                    <ul>
+                        <li class="current"><a href="#">Home</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">For Salers</a></li>
+                        <li><a href="#">Contacts</a></li>
+                    </ul>
+                </div>
+                <div id="search_form">
+                    <form method="get" action="#">
+                        <input type="text" onblur="if(this.value=='') this.value='Search on site';" onfocus="if(this.value=='Search on site') this.value='';" value="Search on site" class="txb_search"/>
+                        <input type="submit" value="Search" class="btn_search"/>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--EOF HEADER-->
+    <!--BEGIN CONTENT-->
 
-		<!--[if (lt IE 9) & (!IEMobile)]>
-			{!! HTML::script('js/vendor/respond.min.js') !!}
-		<![endif]-->
-		<!--[if lt IE 9]>
-			{!! HTML::style('https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js') !!}
-			{!! HTML::style('https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js') !!}
-		<![endif]-->
+        @yield('content')
 
-		{!! HTML::style('http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800') !!}
-		{!! HTML::style('http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic') !!}
+    <!--EOF CONTENT-->
+    <!--BEGIN FOOTER-->
+        @extends('partials.footer')
+    <!--EOF FOOTER-->
+    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script>
+        (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='//www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        ga('create','UA-XXXXX-X');ga('send','pageview');
+    </script>
 
-	</head>
-
-  <body>
-
-	<!--[if lt IE 8]>
-		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-	<![endif]-->
-
-	<header role="banner">
-
-		<div class="brand">{{ trans('front/site.title') }}</div>
-		<div class="address-bar">{{ trans('front/site.sub-title') }}</div>
-		<div id="flags" class="text-center"></div>
-		<nav class="navbar navbar-default" role="navigation">
-			<div class="container">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="index.html">{{ trans('front/site.title') }}</a>
-				</div>
-				<div class="collapse navbar-collapse">
-					<ul class="nav navbar-nav">
-						<li {!! classActivePath('/') !!}>
-							{!! link_to('/', trans('front/site.home')) !!}
-						</li>
-						@if(session('statut') == 'visitor' || session('statut') == 'user')
-							<li {!! classActivePath('contact/create') !!}>
-								{!! link_to('contact/create', trans('front/site.contact')) !!}
-							</li>
-						@endif
-						<li {!! classActiveSegment(1, ['articles', 'blog']) !!}>
-							{!! link_to('articles', trans('front/site.blog')) !!}
-						</li>
-						@if(Request::is('auth/register'))
-							<li class="active">
-								{!! link_to('auth/register', trans('front/site.register')) !!}
-							</li>
-						@elseif(Request::is('password/email'))
-							<li class="active">
-								{!! link_to('password/email', trans('front/site.forget-password')) !!}
-							</li>
-						@else
-							@if(session('statut') == 'visitor')
-								<li {!! classActivePath('auth/login') !!}>
-									{!! link_to('auth/login', trans('front/site.connection')) !!}
-								</li>
-							@else
-								@if(session('statut') == 'admin')
-									<li>
-										{!! link_to_route('admin', trans('front/site.administration')) !!}
-									</li>
-								@elseif(session('statut') == 'redac') 
-									<li>
-										{!! link_to('blog', trans('front/site.redaction')) !!}
-									</li>
-								@endif
-								<li>
-									{!! link_to('auth/logout', trans('front/site.logout')) !!}
-								</li>
-							@endif
-						@endif
-						<li class="dropdown">
-							<a data-toggle="dropdown" class="dropdown-toggle" href="#"><img width="32" height="32" alt="{{ session('locale') }}"  src="{!! asset('img/' . session('locale') . '-flag.png') !!}" />&nbsp; <b class="caret"></b></a>
-							<ul class="dropdown-menu">
-							@foreach ( config('app.languages') as $user)
-								@if($user !== config('app.locale'))
-									<li><a href="{!! url('language') !!}/{{ $user }}"><img width="32" height="32" alt="{{ $user }}" src="{!! asset('img/' . $user . '-flag.png') !!}"></a></li>
-								@endif
-							@endforeach
-							</ul>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-		@yield('header')	
-	</header>
-
-	<main role="main" class="container">
-		@if(session()->has('ok'))
-			@include('partials/error', ['type' => 'success', 'message' => session('ok')])
-		@endif	
-		@if(isset($info))
-			@include('partials/error', ['type' => 'info', 'message' => $info])
-		@endif
-		@yield('main')
-	</main>
-
-	<footer role="contentinfo">
-		 @yield('footer')
-		<p class="text-center"><small>Copyright &copy; Momo</small></p>
-	</footer>
-		
-	{!! HTML::script('//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js') !!}
-	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
-	{!! HTML::script('js/plugins.js') !!}
-	{!! HTML::script('js/main.js') !!}
-
-	<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-	<script>
-		(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-		function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-		e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-		e.src='//www.google-analytics.com/analytics.js';
-		r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-		ga('create','UA-XXXXX-X');ga('send','pageview');
-	</script>
-
-	@yield('scripts')
-
-  </body>
+    @yield('scripts')
+    </body>
 </html>
